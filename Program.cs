4a497e18144d4035902016cedb2fa35d5bd7a9ea
Little-Pink-Dragon <111520046+Little-Pink-Dragon@users.noreply.github.com>
2022-09-08 03:11:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int i;
+ 
+            Console.Write("Введите число: ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("{0} Чётное число", i);
+            }
+            else
+            {
+                Console.Write("{0} Нечётное число", i);
+            }
